b94a28079524fa0088e3e45da5758282bd28d929
Cody Hoover <hoovercj@iastate.edu>
2013-09-07 15:01:35 -0500

    index

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
+import os
+import urllib
 import cgi
 
 from google.appengine.api import users
 
+import jinja2
 import webapp2
 
-MAIN_PAGE_HTML = """\
-<html>
-  <body>
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-"""
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions=['jinja2.ext.autoescape'])
 
 class MainHandler(webapp2.RequestHandler):
    
     def get(self):
-	self.response.write(MAIN_PAGE_HTML)
+
+	user = users.get_current_user()
+
+	if user:
+		url = users.create_logout_url(self.request.uri)
+		url_linktext = 'Logout'
+		guestbook_name = user.nickname()
+	else:
+		url = users.create_login_url(self.request.uri)
+		url_linktext = 'Login'
+	template_values = {
+		'guestbook_name': guestbook_name, 
+		'url': url,
+		'url_linktext': url_linktext,
+	}
+
+	template = JINJA_ENVIRONMENT.get_template('index.html')
+	self.response.write(template.render(template_values))
+	
 
 class Guestbook(webapp2.RequestHandler):
     def post(self):
