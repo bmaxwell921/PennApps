a722f6f26c09ba97555d23236409d7b8a70d76da
Cody Hoover <hoovercj@iastate.edu>
2013-09-07 13:59:41 -0500

    first login screen

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from google.appengine.api import users
+
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
+   
     def get(self):
+	user = users.get_current_user()
+
+	if user:
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write('Hello, ' + user.nickname())
+	else:
+		self.redirect(users.create_login_url(self.request.uri))
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
