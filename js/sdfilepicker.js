be332a86d7f6d83cda6cb89a6e4ecdbc04569aea
Brandon <bmaxwell921@gmail.com>
2013-09-07 23:02:29 -0400

    stuff for curtis

--- js/sdfilepicker.js
+++ js/sdfilepicker.js
@@ -6,16 +6,18 @@
 		ii) if not, send them to the login page
 */
 function attemptLogin() {
-	WL.init({client_id : '0000000040105199', redirect_uri: 'http://soundclouduploader.appspot.com/'});
-	// WL.Event.subscribe("auth.login", onLogin);
-	// WL.Event.subscribe("auth.sessionChange", onSessionChange);
+	WL.Event.subscribe("auth.login", onLogin);
+	WL.Event.subscribe("auth.sessionChange", onSessionChange);
+	
+	WL.init({client_id : '0000000040105199', redirect_uri: 'http://soundclouduploader.appspot.com/',
+		scope: "wl.skydrive", response_type: "token"});
 
-	// var session = WL.getSession();
-	// if (session) {
-	// 	console.log("SD: User is already logged in");
-	// } else {
-	// 	WL.login({scope: "wl.signin"});
-	// }
+	var session = WL.getSession();
+	if (session) {
+		console.log("SD: User is already logged in");
+	} else {
+		WL.login({scope: "wl.signin"});
+	}
 	downloadFiles();
 }
 
@@ -38,19 +40,22 @@ function downloadFiles() {
 		mode: "open",
 		select: "multi"
 	}).then(function(response) {
-	// 	console.log("Doing shit");
-	// 	var data = {};
-	// 	data.numFiles = response.data.files.length;
-	// 	if (response.data.files.length > 0) {
-	// 		for (var file = 0; file < response.data.files.length; ++file) {
-	// 			console.log("Loopin");
-	// 			data["file" + file] = response.data.files[file].id; 
-	// 		}
-	// 	}
-	// 	console.log(data);
-	// 	var url = '/sdUpload';
-	// 	$.post(url, data, function(derter, status) {
-	// 		console.log(derter);
-	// 	});
+		console.log("Doing shit");
+		var data = {};
+		data.numFiles = response.data.files.length;
+		if (response.data.files.length > 0) {
+			for (var file = 0; file < response.data.files.length; ++file) {
+				console.log("Loopin");
+				data["file" + file] = response.data.files[file].id; 
+			}
+		}
+		console.log(data);
+		var url = '/sdUpload';
+		$.post(url, data, function(derter, status) {
+			console.log(derter);
+		});
+	},
+	function (responseFailed) {
+		console.log(responseFailed.error.message);
 	});
 }
